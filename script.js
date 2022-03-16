1ae0056892c457b0aa81b3f8694aaa2c083afc69
jbc14 <jean.baptiste.conte@gmail.com>
2022-03-16 22:28:31 +0100

    Fin grid.js

---
+++ script.js
@@ -0,0 +1,7 @@
+import Grid from './grid.js';
+
+const gameBoard = document.getElementById('game-board');
+
+const grid = new Grid(gameBoard);
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+grid.randomEmptyCell().tile = new Tile(gameBoard);
